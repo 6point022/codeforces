28c19d8b81de1f4097693ba3b6363c2a8e480fad
Anmol Gupta <44764339+6point022@users.noreply.github.com>
2019-06-02 14:28:19 +0530

    Add files via upload
    Execute the file to get the number of solved problems by a user on Codeforces.
    Change the username in the source code before running it.

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+username = 'anmolgupta'
+s = requests.get('http://codeforces.com/api/user.status?handle=' + username).text
+
+if s.find('"status":"FAILED"') != -1:
+	print('Request failed')
+else:
+	print('No. of problems solved by ' + username + ': ' + str(s.count('"verdict":"OK"')))
+
